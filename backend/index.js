8493ebe32e681237bf325ed77f8632959ffff3c3
2100679b <2100679b@umich.mx>
2025-06-04 21:10:07 -0600

    Primera versión del proyecto

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('¡Servidor backend funcionando correctamente!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
