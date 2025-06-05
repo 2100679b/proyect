21aac67ad586f6debae1ea72e14171cd3da2d3a8
2100679b <2100679b@umich.mx>
2025-06-05 11:51:09 -0600

    Cuarta versión del proyecto

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,9 +11,10 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import Cookies from 'js-cookie'  // <-- Importa js-cookie aquí
 
 // 1. Importa explícitamente todos los iconos 'solid' (fas) que uses en tu aplicación
 import { 
@@ -31,13 +32,10 @@ import {
   faCheckDouble, 
   faUser, 
   faSignOutAlt,
-  faSpinner, // Para el spinner de carga
-  faBars,    // Para el icono del menú de hamburguesa
-  faTimes    // Para el icono de cerrar el menú
+  faSpinner,
+  faBars,
+  faTimes
 } from '@fortawesome/free-solid-svg-icons'
-
-// 2. Si quieres usar la versión 'regular' (far) de la campana u otros iconos, impórtalos también:
-// import { faBell as farFaBell } from '@fortawesome/free-regular-svg-icons' 
 
 // 3. Añade todos los iconos importados a la librería de Font Awesome
 library.add(
@@ -58,13 +56,20 @@ library.add(
   faSpinner,
   faBars,
   faTimes
-  // Si importaste farFaBell, añádelo aquí también:
-  // farFaBell
 )
 
+// Crea el plugin para cookies
+const CookiesPlugin = {
+  install: (app) => {
+    app.config.globalProperties.$cookies = Cookies
+  }
+}
+
 const app = createApp(App)
+
 app.use(store)
 app.use(router)
-// Esta línea es CRUCIAL para que el componente <font-awesome-icon> funcione
+app.use(CookiesPlugin) // <-- Usa el plugin de cookies aquí
+
 app.component('font-awesome-icon', FontAwesomeIcon) 
 app.mount('#app')
