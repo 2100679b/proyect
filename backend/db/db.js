a6b4953a199f6eb17ec981f264ae2b0b4a6c1c35
2100679b <2100679b@umich.mx>
2025-06-07 00:35:36 -0600

    Tercera versión del proyecto

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,18 @@
+// db/db.js
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'tu_usuario',
-  host: 'localhost',
-  database: 'tu_basededatos',
-  password: 'tu_contraseña',
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }, // necesario para RDS/AWS
 });
+
+pool.connect()
+  .then(() => console.log('✅ Conectado a PostgreSQL'))
+  .catch(err => console.error('❌ Error de conexión', err));
 
 module.exports = pool;
